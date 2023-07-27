57fcddc95e52dc33a5c91ea17ea049d7714d85a6
Thales Cristian <thales.cristianeugenio@gmail.com>
2023-07-26 22:20:30 -0300

    Inscricao

---
+++ inscricao-upskilling/inscricao-upskilling/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using inscricao_upskilling.Models;
+using Npgsql;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace inscricao_upskilling.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About(PessoaModel pessoa)
+        {
+            string Nome = pessoa.Nome;
+            string Cpf = pessoa.Cpf;
+            string Celular = pessoa.Celular;
+            string Email = pessoa.Email;
+            string Login = pessoa.Login;
+            string Senha = pessoa.Senha;
+
+            string connectionString = "Host=localhost;Port=54321;Username=postgres;Password=postgres;Database=UpskillingGrupo4Final";
+
+            int PessoaId;
+            int AlunoId;
+
+
+            using (NpgsqlConnection con = new NpgsqlConnection(connectionString))
+            {
+                con.Open();
+                string comando = $"INSERT INTO public.\"Pessoas\"(\"Nome\", \"CPF\", \"Celular\", \"Email\")VALUES('{Nome}', '{Cpf}', '{Celular}', '{Email}') RETURNING \"Id\"";
+                using (NpgsqlCommand cmd = new NpgsqlCommand(comando, con))
+                {
+                    cmd.CommandText = comando;
+                    cmd.Parameters.AddWithValue(comando);
+                    PessoaId = (int)cmd.ExecuteScalar();
+                }
+
+                comando = $"INSERT INTO public.\"Alunos\"(\"PessoaId\", \"Matricula\", \"DataCadastro\")VALUES('{PessoaId}', '{PessoaId}2023{PessoaId}', 'NOW()') RETURNING \"Id\"";
+                using (NpgsqlCommand cmd = new NpgsqlCommand(comando, con))
+                {
+                    cmd.CommandText = comando;
+                    cmd.Parameters.AddWithValue(comando);
+                    AlunoId = (int)cmd.ExecuteScalar();
+                }
+
+                comando = $"INSERT INTO public.\"Usuarios\"(\"PessoaId\", \"Login\", \"Senha\", \"DataUltimoAcesso\", \"DataCadastro\", \"AlunoId\", \"PerfilAdministrativo\", \"PerfilAluno\") VALUES('{PessoaId}', '{Login}', '{Senha}', 'NOW()', 'NOW()', {AlunoId}, false, true)";
+                using (NpgsqlCommand cmd = new NpgsqlCommand(comando, con))
+                {
+                    cmd.Parameters.AddWithValue(comando);
+                    cmd.ExecuteNonQuery();
+                }
+                con.Close();
+                
+                return View();
+
+            }
+         }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+    }
+
+
+}
---
+++ inscricao-upskilling/inscricao-upskilling/Models/PessoaModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace inscricao_upskilling.Models
+{
+    public class PessoaModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Celular { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+    }
+}
